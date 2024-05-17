1adeaa670b9730f854fda077c2880e52f859cf4c
Alb Phantom <166353154+Leone800@users.noreply.github.com>
2024-05-17 12:27:53 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+const app = document.querySelector(".adicionais")
+const add = adicionais.querySelector(".add")
+const addInput = adicionais.querySelector(".busca")
+const confirmar = adicionais.querySelector(".confirm")
+
+const url = {id:"https, socket, http", image:""}
+const div = document.createElement("adicionais")
+
+url.id = crypto.randomUUID()
+url.image = addInput.value()
+
+function confirm(){
+  async 
+}
